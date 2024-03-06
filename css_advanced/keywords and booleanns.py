d9660d9c7965e0e9132c505ec43f2c6b983699e1
Uchegbu <92696988+Uchegbu@users.noreply.github.com>
2024-03-06 23:32:36 +0100

    at last

---
+++ css_advanced/keywords and booleanns.py
@@ -0,0 +1,40 @@
+#booleans values true or false
+
+#print(41 == 41)
+
+#decisions
+#comparison operators
+#voting_limit = 18
+
+ #age = int(input("How old are you? "))
+
+#if age >= voting_limit:
+#    print("you can vote") 
+#else:
+#    print("check when you are 18+")
+
+#logical operators and,or,not
+#VOTING_LIMIT = 18
+#HEIGHT_LIMIT = 6
+#age = int(input("How old are you? "))
+#height = int(input("What is your height in fts? "))
+
+#if age >= VOTING_LIMIT and height == HEIGHT_LIMIT:
+#    print("you can vote") 
+#else:
+#    print("check when you are 18+")
+
+#identity operstors is,is not
+#VOTING_LIMIT = 18
+#HEIGHT_LIMIT = 6
+#age = int(input("How old are you? "))
+#height = int(input("What is your height in fts? "))
+
+#if age > VOTING_LIMIT and height > HEIGHT_LIMIT:
+#    print("you can vote")
+#elif age == VOTING_LIMIT and  height == HEIGHT_LIMIT :
+#    print("you are a perfect a match") 
+#else:
+#    print("check when you are 18+")
+
+#loops types: while and for
---
+++ css_advanced/python.py
@@ -0,0 +1,68 @@
+#print("hello world")
+#print("bright") 
+#print('uchegbu bright')
+
+#name = "uchegbu bright"
+#age = 30
+#location = "south africa"
+
+#print(type(age))
+
+#print("name: " + name )
+#print("age:" + str(age))
+#print("location: " + location)
+
+#age = 15
+#height = 11.8
+imaginary = 3j
+
+name  = "Bright Uchegbu"
+size = len(name)
+capital = name.upper()   
+lowercase = name.lower()
+rep = name.replace("Bright","kelvin")
+
+slice = name[7:]
+
+isTrue = True
+
+print(5>10)  
+
+#print(rep + " has " + str(size) + " characters")
+#print("{} has {} characters".format(rep, size))
+
+age = 10
+age += 3
+print(age)
+ 
+
+#name = input("please enter your name")
+#age =  input("please enter your age")
+#print("Hello " + name)
+#print("you are {} years old".format(age))
+
+#name = input("enter your name: ")
+#width = float(input("enter the width of your room in meters"))
+#length = float(input("enter the length of your room in meters"))
+#compute the area of the room
+#area = length*width
+
+#area = length * width   
+
+#print("Hello{}".format(name))
+#print("The area of yourroom is {}".format(area))
+
+"""
+functions
+"""
+
+def helloworld():
+    print("uchegbu bright")
+
+#invoke the function
+helloworld() 
+
+def howdy(user):
+    print("Howdy {}".format(user))
+
+howdy(user="silas")
